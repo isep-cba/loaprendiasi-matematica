e3aaaefe8cf70b31d3d2867b2af6045fc1a96738
moviedo compu isep <moviedo@isep-cba.edu.ar>
2024-09-02 13:35:18 -0300

    corrige scripts

--- scripts.js
+++ scripts.js
@@ -8,10 +8,8 @@ var btn = document.getElementById("myBtn");
 function myFunction() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
   } else {
     video.pause();
-    btn.innerHTML = "Play";
   }
 }
 
