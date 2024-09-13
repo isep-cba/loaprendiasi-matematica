d62dcc81084ae40702b884b44c5bf9a0b2ce4f81
moviedo <moviedo@isep-cba.edu.ar>
2024-09-13 17:21:25 -0300

    Offcanvas y rotacion en mobile portrait

--- scripts.js
+++ scripts.js
@@ -14,3 +14,11 @@ function myFunction() {
 }
 
 
+/* Sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
